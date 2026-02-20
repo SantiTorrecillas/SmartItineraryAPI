e1a1333aafc56c90643769983a5b6039bc4c4237
SantiTorrecillas <santiagotorrecillas99@gmail.com>
2026-02-20 16:12:37 -0300

    add deployed frontend origin

--- SmartItineraryAPI/Program.cs
+++ SmartItineraryAPI/Program.cs
@@ -24,7 +24,8 @@ builder.Services.AddCors(options =>
         {
             policy.WithOrigins(
                 "http://localhost:5173",
-                "http://localhost:4173"
+                "http://localhost:4173",
+                "https://smart-itinerary-front-end.vercel.app/"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
