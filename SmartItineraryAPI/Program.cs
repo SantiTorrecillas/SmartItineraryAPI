f6885793199c094ce8572bfdf8262bccced3a148
SantiTorrecillas <santiagotorrecillas99@gmail.com>
2026-02-20 16:16:06 -0300

    fix

--- SmartItineraryAPI/Program.cs
+++ SmartItineraryAPI/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
             policy.WithOrigins(
                 "http://localhost:5173",
                 "http://localhost:4173",
-                "https://smart-itinerary-front-end.vercel.app/"
+                "https://smart-itinerary-front-end.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
